f0b3f6abf5345be982eb69ae598c0ff9450c2e39
Semen Kostenko <semen.kostenko@eliftech.com>
2025-08-07 14:57:24 +0300

    fix: stage 9

--- utils/loginItems.ts
+++ utils/loginItems.ts
@@ -29,7 +29,7 @@ export const loginItems: LoginItem[] = [
     accentColorClass: 'bg-[#F6F6F6]',
     items: [
       { label: 'Payment Gateway', link: 'https://quaife.ctpe.info' },
-      { label: 'Onboarding', link: 'https://quaife.moxo.com/' },
+      { label: 'Onboarding', link: 'https://qashpay.moxo.com/' },
       { label: 'Agent Portal', link: 'https://agentportal.qashpay.com' },
       { label: 'Dispute Management', link: 'https://disputes.qashpay.com/' },
       { label: 'PCI and ASV Scanning', link: 'https://pci.qashpay.com' },
